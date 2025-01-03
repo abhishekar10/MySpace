ecd0a97ccd9e25036290086a7b5168b37ff26c32
Abhishek A R <getarabhishek@gmail.com>
2025-01-03 19:02:02 +0530

    Added loader and music-locker setup

--- JS/weather.js
+++ JS/weather.js
@@ -1,3 +1,7 @@
+const loadingMessage = document.getElementById("message1");
+const errorMessage = document.getElementById("message2");
+const temperatureSection = document.getElementById("message3");
+
 const fetchWeatherData = async (latitude, longitude) => {
     const apiKey = '9db02983ca97c4017646c34ffa398122';
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
@@ -5,11 +9,11 @@ const fetchWeatherData = async (latitude, longitude) => {
         const response = await fetch(url);
         const data = await response.json();
         const temperature = data.main.temp;
-        console.log(data.main.temp);
+        console.log(temperature);
         const rain = data.rain ? data.rain['1h'] : 0;
         console.log(rain);
         const place = data.name;
-        console.log(data.name);
+        console.log(place);
         // updateWeatherDisplay(temperature, rain, place);
     } catch (error) {
         console.error('Error fetching weather data:', error);
